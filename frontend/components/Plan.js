ad058ca0cc575863bbea4937edc10d0008f61f41
Marc Adnin <m_adnin@etu-webschoolfactory.fr>
2019-07-04 21:58:20 +0200

    .

--- frontend/components/Plan.js
+++ frontend/components/Plan.js
@@ -11,7 +11,7 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Layout from '../components/MyLayout.js'
+import Layout from './MyLayout.js'
 
 function MadeWithLove() {
   return (
